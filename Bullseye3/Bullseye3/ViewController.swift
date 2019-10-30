1a8254ac541ad8d3bfffecdc94c815442bca0e3b
Gonzalo Gamez <takosaga@gmail.com>
2019-10-29 20:30:33 -0500

    Moving Richards Project to another MacBook

---
+++ Bullseye3/Bullseye3/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Bullseye3
+//
+//  Created by Gonzalo Gamez on 10/28/19.
+//  Copyright © 2019 Gamez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+        
+    @IBAction func showKNOCKKnock(){
+            let alert = UIAlertController(title: "Hello", message: "Who's there?", preferredStyle: .actionSheet)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+            present(alert, animated: true, completion: nil)
+            
+        }
+
+    }
+
+
+
